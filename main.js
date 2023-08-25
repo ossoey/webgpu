59c68e0439f2cec7c194fdbb38cf6fca03195255
JJ Ebanga <ebanga@ossoey.com>
2023-08-24 23:43:19 -0400

    Simple Gradient triangle

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
 
 window.addEventListener(`load`, ()=>{
 
-
     import(`./modules/webgpuprinciple.js`).then((module)=>{
 
         module.functions_entries[0].func();
-
         let list = document.createElement(`select`);
-        document.querySelector(`#menu`).append(list);
 
+        document.querySelector(`#menu`).append(list);
         module.functions_entries.forEach((elt,index)=>{
 
             let subElt = document.createElement(`option`);
-        
             subElt.innerHTML = elt.desc;
             subElt.functionId = index;
             subElt.setAttribute(`id`, index);
             list.appendChild(subElt);
-
         });
 
         list.addEventListener(`change`,(event)=>{
@@ -27,5 +23,4 @@ window.addEventListener(`load`, ()=>{
 
     });
 
-
-})
+});
